f42c7779e0ac89f129c3e22335cc55328bd6caec
RPatroch <reza0000835@gmail.com>
2023-09-20 11:14:59 +0330

    Migrations

--- Url Shortener/Migrations/20230920073521_addUser.Designer.cs
+++ Url Shortener/Migrations/20230920073521_addUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Url_Shortener;
 
@@ -10,9 +11,11 @@ using Url_Shortener;
 namespace Url_Shortener.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20230920073521_addUser")]
+    partial class addUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
