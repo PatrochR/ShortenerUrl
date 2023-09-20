3de12b6a946d2de776693783705b41612eded291
RPatroch <reza0000835@gmail.com>
2023-09-20 11:14:47 +0330

    Create User Service

---
+++ Url Shortener/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using Url_Shortener.Entities;
+
+namespace Url_Shortener.Services
+{
+    public interface IUserService
+    {
+        Task<bool> Register(string username , string password);
+
+    }
+}
---
+++ Url Shortener/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using Url_Shortener.Entities;
+
+namespace Url_Shortener.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationContext _context;
+
+        public UserService(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Register(string username , string password)
+        {
+            var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+            var user = new User
+            {
+                PasswordHash = passwordHash,
+                UserName = username,
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
