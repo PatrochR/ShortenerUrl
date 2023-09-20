e3c79013380ab68001d1635b3d7d0882389945b9
RPatroch <reza0000835@gmail.com>
2023-09-20 11:15:18 +0330

    Create Validator

---
+++ Url Shortener/Validation/RegisterRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Url_Shortener.Models;
+
+namespace Url_Shortener.Validation
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(r=>r.Username).NotEmpty().MinimumLength(3);
+            RuleFor(r=>r.Password).NotEmpty().MinimumLength(8);
+        }
+    }
+}
