30a4fc72438997ceedc96a63e717df3495b7997b
RPatroch <reza0000835@gmail.com>
2023-09-24 10:49:55 +0330

    Create Validator

---
+++ Url Shortener/Validation/LoginRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Url_Shortener.Models;
+
+namespace Url_Shortener.Validation
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(l => l.Username).NotEmpty().MinimumLength(3);
+            RuleFor(l => l.Password).NotEmpty().MinimumLength(8);
+        }
+    }
+}
