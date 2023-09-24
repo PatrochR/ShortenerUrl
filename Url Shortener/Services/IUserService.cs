ba2ddc7a0c5dd43d4b9415e88cc3c60fb08addf7
RPatroch <reza0000835@gmail.com>
2023-09-24 10:49:39 +0330

    Add Login to UserService

--- Url Shortener/Services/IUserService.cs
+++ Url Shortener/Services/IUserService.cs
@@ -5,6 +5,9 @@ namespace Url_Shortener.Services
     public interface IUserService
     {
         Task<bool> Register(string username , string password);
+        bool Login(User user, out string token);
+
+        Task<User?> FindUser(string username);
 
     }
 }
--- Url Shortener/Services/UserService.cs
+++ Url Shortener/Services/UserService.cs
@@ -1,4 +1,9 @@
-﻿using Url_Shortener.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Url_Shortener.Entities;
 
 namespace Url_Shortener.Services
 {
@@ -11,7 +16,27 @@ namespace Url_Shortener.Services
             _context = context;
         }
 
-        public async Task<bool> Register(string username , string password)
+        public async Task<User?> FindUser(string username) => await _context.Users.SingleOrDefaultAsync(u => u.UserName == username);
+
+        public bool Login(User user, out string token)
+        {
+            var claims = new List<Claim> { 
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name , user.UserName)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("IF I LOSE IT ALL . SLIP AND FALL I NEVEBR LOOK AWAY IF I LOSE IT ALL LOSE IT ALL LOSE IT ALL IF I LOSE IT ALL OUTSIDE THE WALL "));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var jwttoken = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+            var jwt = new JwtSecurityTokenHandler().WriteToken(jwttoken);
+            token = jwt;
+            return true;
+        }
+
+        public async Task<bool> Register(string username, string password)
         {
             var passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
             var user = new User
